45648a73d676407d70211c5014dd9914be594a9c
Pamela Prosperi <pamepros@gmail.com>
2013-08-09 11:47:33 -0300

    now works with cordova 2.5

--- www/pg-plugin-screen-orientation.js
+++ www/pg-plugin-screen-orientation.js
@@ -1,6 +1,6 @@
 var screenOrientation = function() {}
 
 screenOrientation.prototype.set = function(str, success, fail) {
-    PhoneGap.exec(success, fail, "ScreenOrientation", "set", [str]);
+	cordova.exec(null, null, "ScreenOrientation", "set", [str]);
 };
 navigator.screenOrientation = new screenOrientation();
